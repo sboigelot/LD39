bffb358327305803c541a577d5cfa8c3891c1988
Simon Boigelot <sboigelot@gmail.com>
2017-07-31 02:25:14 +0200

    Stop monster panel blink on redraw

--- Thirst/Assets/Scripts/Controllers/MonstersPanelController.cs
+++ Thirst/Assets/Scripts/Controllers/MonstersPanelController.cs
@@ -49,6 +49,8 @@ namespace Assets.Scripts.Controllers
             {
                 var monster = monsters.FirstOrDefault();
                 monsterPanelController.Redraw(monster);
+                var image = monsterPanelController.GetComponent<Image>();
+                image.color = Color.white;
                 if (monsters.Any())
                 {
                     monsters.RemoveAt(0);
